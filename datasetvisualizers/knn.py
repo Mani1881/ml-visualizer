c07875b8a9b37b3c1ee5b3f5e8fddfc66901ede1
maneesha <manumumba1881@gmail.com>
2024-12-05 15:35:20 +0530

    vmaneesha

---
+++ datasetvisualizers/knn.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import neighbors
+from mlxtend.plotting import plot_decision_regions
+from variables import INPUT_CSV,OUTPUT_PNG
+
+def knn_comparison(data, k):
+    x = data[['X', 'Y']].values
+    y = data['Z'].astype(int).values
+    clf = neighbors.KNeighborsClassifier(n_neighbors=k)
+    clf.fit(x, y)
+    # Plotting decision region
+    plot_decision_regions(x, y, clf=clf, legend=2)
+    # Adding axes annotations
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('KNN with K=' + str(k))
+    plt.savefig(OUTPUT_PNG)  # Change the extension as needed (e.g., .jpg, .jpeg, .svg)
+    plt.show()
+    return
+
+data = pd.read_csv(INPUT_CSV)
+if 'X' in data.columns:
+    x = data['X'].values
+else:
+    x = np.zeros(len(data))  # Or any other default value
+
+if 'Y' in data.columns:
+    y = data['Y'].values
+else:
+    y = np.zeros(len(data))  # Or any other default value
+
+# Rest of your code...  
+for k in [40]:
+    knn_comparison(data, k)
